b27007268c58c1a0aa4798239ca48b56f1636d77
paulozag <ozag@comcast.net>
2016-11-26 18:02:01 -0800

    removes redundant associations for #members for classes Round and Mission

--- resistance/app/models/round.rb
+++ resistance/app/models/round.rb
@@ -2,6 +2,5 @@ class Round < ActiveRecord::Base
   belongs_to :leader, class_name: :Player
   belongs_to :mission
   has_many :team_mbrs, as: :team_mbrable
-  # has_many :members, class_name: "Player", through: :team_mbrs, source: :player
-  has_many :members, class_name: "Player", through: :team_mbrs
+  has_many :members, through: :team_mbrs
 end
