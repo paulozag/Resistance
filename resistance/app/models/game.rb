1801dc3fecdf53824d86fb7d8983cb041975e2f1
paulozag <ozag@comcast.net>
2016-11-26 22:39:00 -0800

    creates MISSION_HASH constant in Game, storing mission numbers and double fail values

--- resistance/app/models/game.rb
+++ resistance/app/models/game.rb
@@ -1,4 +1,43 @@
 class Game < ActiveRecord::Base
   has_many :missions
   has_many :players
+
+  MISSION_HASH  = {
+          5 => {  mission_1: {  member_count: 2, double_fail: false},
+                  mission_2: {  member_count: 3, double_fail: false},
+                  mission_3: {  member_count: 2, double_fail: false},
+                  mission_4: {  member_count: 3, double_fail: false},
+                  mission_5: {  member_count: 3, double_fail: false}
+               },
+          6 => {  mission_1: {  member_count: 2, double_fail: false},
+                  mission_2: {  member_count: 3, double_fail: false},
+                  mission_3: {  member_count: 4, double_fail: false},
+                  mission_4: {  member_count: 3, double_fail: false},
+                  mission_5: {  member_count: 4, double_fail: false}
+               },
+          7 => {  mission_1: {  member_count: 2, double_fail: false},
+                  mission_2: {  member_count: 3, double_fail: false},
+                  mission_3: {  member_count: 3, double_fail: false},
+                  mission_4: {  member_count: 4, double_fail: true},
+                  mission_5: {  member_count: 4, double_fail: false}
+               },
+          8 => {  mission_1: {  member_count: 3, double_fail: false},
+                  mission_2: {  member_count: 4, double_fail: false},
+                  mission_3: {  member_count: 4, double_fail: false},
+                  mission_4: {  member_count: 5, double_fail: true},
+                  mission_5: {  member_count: 5, double_fail: false}
+               },
+          9 => {  mission_1: {  member_count: 3, double_fail: false},
+                  mission_2: {  member_count: 4, double_fail: false},
+                  mission_3: {  member_count: 4, double_fail: false},
+                  mission_4: {  member_count: 5, double_fail: true},
+                  mission_5: {  member_count: 5, double_fail: false}
+               },
+          10 => {  mission_1: {  member_count: 3, double_fail: false},
+                   mission_2: {  member_count: 4, double_fail: false},
+                   mission_3: {  member_count: 4, double_fail: false},
+                   mission_4: {  member_count: 5, double_fail: true},
+                   mission_5: {  member_count: 5, double_fail: false}
+                }
+  }
 end
