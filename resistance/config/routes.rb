0741a65088fe49cde2bdbfaaf555a5eda0f90c77
paulozag <ozag@comcast.net>
2016-12-13 15:53:10 -0800

    adds users controller, routing for users and sessions

--- resistance/config/routes.rb
+++ resistance/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
+
+  resources :users, only: [:new, :create]
+
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
