a330b3a9e346365167350413b3f7ad79807d6f12
paulozag <ozag@comcast.net>
2016-12-15 08:43:49 -0800

    adds user create and new functionality, sessions controller

---
+++ resistance/app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+  def new
+    p "*" * 80
+    p "in sessions new"
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- resistance/app/controllers/users_controller.rb
+++ resistance/app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
+    p params[:user]
+    @user = User.create(user_params)
 
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :age)
   end
 
 end
