cba9a5256c5b9bc912122fd8fc97f77a8d801121
paulozag <ozag@comcast.net>
2017-01-09 10:11:36 -0800

    re-adds columns to games

--- resistance/db/schema.rb
+++ resistance/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109180129) do
+ActiveRecord::Schema.define(version: 20170109180944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170109180129) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.integer  "creator_id"
+    t.string   "game_name"
   end
 
   create_table "missions", force: :cascade do |t|
