63ab0bd0d47349263cd72a7d07e8e0c1bfb91aab
paulozag <ozag@comcast.net>
2017-01-09 13:22:04 -0800

    adds game_key field to games

--- resistance/db/schema.rb
+++ resistance/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109180944) do
+ActiveRecord::Schema.define(version: 20170109211907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170109180944) do
     t.datetime "updated_at",                null: false
     t.integer  "creator_id"
     t.string   "game_name"
+    t.string   "game_key"
   end
 
   create_table "missions", force: :cascade do |t|
