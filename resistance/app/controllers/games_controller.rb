05b289e0442810da1424185705f75194dca8ec82
paulozag <ozag@comcast.net>
2017-01-09 16:20:34 -0800

    creates initial player with game creation, creates game

--- resistance/app/controllers/games_controller.rb
+++ resistance/app/controllers/games_controller.rb
@@ -9,16 +9,24 @@ class GamesController < ApplicationController
   end
 
   def create
+    player = create_player
     @game = current_user.games.create(game_params)
+    player.game = @game
+    @game.players << player
+
     redirect_to game_path(id: @game.id)
   end
 
   def show
-    "in game show"
+    @game = Game.find(params[:id])
   end
 
   private
   def game_params
     params.require(:game).permit(:game_name, :game_key)
+  end
+
+  def create_player
+    player = current_user.players.create(name: params[:creator_nickname])
   end
 end
--- resistance/app/controllers/welcome_controller.rb
+++ resistance/app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
 
   def index
-    helper_test
   end
 end
