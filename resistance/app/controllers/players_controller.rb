74bbd3ff4be93f63d8262817b3f31fa480bdfca9
paulozag <ozag@comcast.net>
2017-01-10 09:51:32 -0800

    creates new player instance in join game form submission from game#show

--- resistance/app/controllers/players_controller.rb
+++ resistance/app/controllers/players_controller.rb
@@ -4,6 +4,11 @@ class PlayersController < ApplicationController
 
   def create
     p '&*&' *25
+    p params
 
+    player =  Player.create(
+      name:       params[:player_nickname],
+      user_id:    current_user.id,
+      game_id:    params[:game_id])
   end
 end
