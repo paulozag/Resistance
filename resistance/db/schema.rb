365d2e0177a5e9f1fddc070c0c71d0c0bda68f8b
paulozag <ozag@comcast.net>
2017-01-17 12:34:06 -0800

    adds playable boolean field to games, adds start game button to game room

--- resistance/db/schema.rb
+++ resistance/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109211907) do
+ActiveRecord::Schema.define(version: 20170117201719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170109211907) do
     t.integer  "creator_id"
     t.string   "game_name"
     t.string   "game_key"
+    t.boolean  "playable"
   end
 
   create_table "missions", force: :cascade do |t|
