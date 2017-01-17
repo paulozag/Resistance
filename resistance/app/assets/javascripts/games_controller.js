518ffc82349ca510ca215855559e1205dfe89f7b
paulozag <ozag@comcast.net>
2017-01-17 11:34:09 -0800

    creates controlle for game upon hitting game_room.erb

--- resistance/app/assets/javascripts/games_controller.js
+++ resistance/app/assets/javascripts/games_controller.js
@@ -1,3 +1,12 @@
 var gamesController = function(data){
   console.log('in gc function')
 }
+
+var GameController = function(gameData){
+  this.gameID = gameData.gameID;
+  this.playerID = gameData.playerID;
+}
+
+GameController.prototype.receive = function(data){
+  console.log('receiving message from server broadcast')
+}
