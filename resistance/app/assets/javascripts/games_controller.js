b87a076b1a9577a42ea62e7ada01c840ced21220
paulozag <ozag@comcast.net>
2017-01-17 16:03:35 -0800

    adds addPlayer prtotype function to GameController

--- resistance/app/assets/javascripts/games_controller.js
+++ resistance/app/assets/javascripts/games_controller.js
@@ -6,4 +6,16 @@ var GameController = function(gameData){
 
 GameController.prototype.receive = function(data){
   console.log('receiving message from server broadcast')
+  switch(data.action){
+    case 'addPlayer':
+      this.addPlayer(data);
+      break;
+    default:
+      console.log('in receive action with no action accounted for')
+      break;
+  }
+}
+
+GameController.prototype.addPlayer = function(data){
+
 }
