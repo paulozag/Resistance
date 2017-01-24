3abac9281fc50c3cd1c8ebf1273b1f32fd8a1ebe
paulozag <ozag@comcast.net>
2017-01-24 10:19:41 -0800

    removes playable field from Game

--- resistance/db/migrate/20161126192238_create_games.rb
+++ resistance/db/migrate/20161126192238_create_games.rb
@@ -2,7 +2,6 @@ class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
       t.boolean :joinable, default: true
-      t.boolean :playable, default: false
       t.references :creator
 
       t.timestamps null: false
